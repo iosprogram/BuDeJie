8f210b5157d7b31ccf506ff4ef1b78286817495c
马红伟 <mahongwei@mahongweidemacbook-pro.local>
2017-05-30 13:36:56 +0800

    初始化项目

--- BuDeJie/BuDeJieTests/BuDeJieTests.m
+++ BuDeJie/BuDeJieTests/BuDeJieTests.m
@@ -3,7 +3,7 @@
 //  BuDeJieTests
 //
 //  Created by 马红伟 on 17/5/30.
-//  Copyright © 2017年 马红伟. All rights reserved.
+//  Copyright © 2017年 MHW. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
